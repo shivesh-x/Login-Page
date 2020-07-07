eba9af0d015ee82dbc513bf4586844af31972dfe
Sumitk504 <65806501+Sumitk504@users.noreply.github.com>
2020-07-08 01:52:05 +0530

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,26 @@
+from tkinter import *
+def LoginPage():
+    login_screen=Tk()
+    login_screen.title("Ultra_Animation")
+    login_screen.geometry("400x300")
+
+    
+    
+    Label(login_screen, text="Enter Login Details").pack()
+    Label(login_screen, text="____________________").pack()
+    Label(login_screen, text="").pack()   
+    Label(login_screen, text="Username").pack()  
+    
+    username_enter = Entry(login_screen,textvariable="username")
+    username_enter.pack()
+    
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password").pack() 
+    
+    password_enter = Entry(login_screen,textvariable="password", show= '*')
+    password_enter.pack()
+    
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=15, height=1).pack()
+    login_screen.mainloop()
+LoginPage()
